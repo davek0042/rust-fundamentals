707777dff4d1f36da1874f5c210b0c85e386d260
David Kleiner <davek@davidkleiner.org>
2025-12-07 18:01:59 -0500

    commit all

--- examples/2-variables/mutable/src/main.rs
+++ examples/2-variables/mutable/src/main.rs
@@ -1,9 +1,9 @@
-
 fn main() {
     let mut message = String::from("Name: Alfredo, Height: ");
     message.clear();
     let mut height = 190;
     height = 189;
+    message = String::from(r#"Name: David, Height: "#);
     println!("{}{}", message, height);
 
 }
