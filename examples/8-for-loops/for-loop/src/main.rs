dba1afbdd7486881a3ae7e9cf3fbdee28611a87e
David Kleiner <davek@davidkleiner.org>
2025-12-08 21:42:20 -0500

    add some working for loops for testing

--- examples/8-for-loops/for-loop/src/main.rs
+++ examples/8-for-loops/for-loop/src/main.rs
@@ -8,9 +8,18 @@ fn main() {
     //     println!("{}", i);
     // }
     
-    let numbers = vec![1, 2, 3, 4, 5];
-    for n in numbers {
-        println!("{}", n);
+    // let numbers = vec![1, 2, 3, 4, 5];
+    // for n in numbers {
+    //     print!("{}, ", n);
+    // }
+    // print!("\n");
+
+    for i in (0..=40).step_by(2) {
+        println!("{},", i);
     }
+
+    // for i in (5..=10).rev() {
+    //     println!("{},", i);
+    // }
 
 }
