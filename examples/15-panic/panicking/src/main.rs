4a17921b3adc36ab4fd90d2d279d3ed7f7bd0374
David Kleiner <davek@davidkleiner.org>
2025-12-09 20:33:36 -0500

    added errors to loop_and_check

--- examples/15-panic/panicking/src/main.rs
+++ examples/15-panic/panicking/src/main.rs
@@ -1,13 +1,30 @@
-fn loop_and_panic(numbers: Vec<i32>) {
+fn loop_and_check(numbers: Vec<i32>) -> Result<(), Vec<String>> {
+    let mut errors = Vec::new();
+    
     for num in numbers {
         if num < 0 {
-            panic!("Negative number found!");
+            errors.push(format!("Negative number found: {}", num));
+        } else {
+            println!("Number: {}", num);
         }
-        println!("Number: {}", num);
+    }
+    
+    if errors.is_empty() {
+        Ok(())
+    } else {
+        Err(errors)
     }
 }
 
 
 fn main() {
-    loop_and_panic(vec![1, 2, 3, 4, -5]);
+    match loop_and_check(vec![1, 2, -3, 4, -5]) {
+        Ok(_) => println!("All numbers processed successfully!"),
+        Err(errors) => {
+            println!("Encountered {} error(s):", errors.len());
+            for error in errors {
+                println!("  - {}", error);
+            }
+        }
+    }
 }
